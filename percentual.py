48e79f1de350605683508678ce705b92e93e51da
João Vitor <162760378+requeijhon@users.noreply.github.com>
2024-08-31 19:08:02 -0300

    Delete percentual.py

--- percentual.py
+++
@@ -1,8 +0,0 @@
-faturamento = [67836.43, 36678.66, 29229.88, 27165.48, 19849.53]
-estados = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espírito Santo', 'Outros estados']
-
-total = sum(faturamento)
-
-for i in range(5):
-    percentual = (faturamento[i]/total)*100
-    print (f'O faturamento de {estados[i]} foi de: {percentual:.2f}%')
